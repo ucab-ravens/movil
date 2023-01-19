448142c93768a9c0dc9aaa66d957f7977e093207
RaynoldG <rrguerrerop@gmail.com>
2023-01-19 00:37:31 -0400

    refresh_page_state

--- lib/modules/courses/views/course_list_view.dart
+++ lib/modules/courses/views/course_list_view.dart
@@ -1,33 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:movil/modules/courses/domain/course.dart';
 import 'package:movil/modules/courses/views/course_card_view.dart';
 import 'package:movil/modules/courses/views/course_provider.dart';
 import 'package:provider/provider.dart';
 
-class CourseListView extends StatelessWidget {
-  const CourseListView({Key? key}) : super(key: key);
+class CourseListView extends StatefulWidget {
+  const CourseListView({super.key});
 
   @override
+  State<CourseListView> createState() => _CourseListViewState();
+}
+
+class _CourseListViewState extends State<CourseListView> {
+  @override
   Widget build(BuildContext context) {
-    final courses =
-        context.select((CourseProvider provider) => provider.courses);
+    var courses = context.select((CourseProvider provider) => provider.courses);
 
-    if (courses == null) {
-      return const Center(child: CircularProgressIndicator());
+    Future<void> pullRefresh() async {
+      await Future.delayed(const Duration(seconds: 2), () {
+        var coursesRefresh =
+            Provider.of<CourseProvider>(context, listen: false).courses;
+        courses = null;
+        //print(courses);
+        setState(() {
+          courses = coursesRefresh;
+          //print(courses);
+          //listCourses(courses);
+        });
+      });
     }
 
-    if (courses.isEmpty) {
-      return const Center(
-          child: Image(image: AssetImage('lib/assets/no_courses.png')));
-    }
+    return Flexible(
+        child: RefreshIndicator(
+            onRefresh: pullRefresh, child: listCourses(courses)));
+  }
 
-    return Expanded(
-      child: ListView.builder(
-        itemCount: courses.length,
-        itemBuilder: (context, index) {
-          final course = courses.elementAt(index);
-          return CourseCard(course);
-        },
-      ),
-    );
+  Widget listCourses(courses) {
+    return (courses == null)
+        ? ListView(
+            padding: const EdgeInsets.only(top: 300),
+            physics: const AlwaysScrollableScrollPhysics(),
+            children: const [
+              Center(child: CircularProgressIndicator()),
+            ],
+          )
+        : (courses!.isEmpty)
+            ? ListView(
+                padding: const EdgeInsets.only(top: 300),
+                physics: const AlwaysScrollableScrollPhysics(),
+                children: const [
+                  Center(
+                      child: Image(
+                          image: AssetImage('lib/assets/no_courses.png'))),
+                ],
+              )
+            : ListView.builder(
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemCount: courses!.length,
+                itemBuilder: (context, index) {
+                  final course = courses?.elementAt(index);
+                  return CourseCard(course!);
+                },
+              );
   }
 }
